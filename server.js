c0c5dbd3f77e3e98be482139dc8d5ece4b4b7abc
Yvonne <yowantings@gmail.com>
2024-05-12 13:29:20 -0600

    render issues

--- server.js
+++ server.js
@@ -87,10 +87,10 @@ app.delete('/api/notes/:id', async (req, res) => {
     }
 });
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
   );
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
   );
   
    
