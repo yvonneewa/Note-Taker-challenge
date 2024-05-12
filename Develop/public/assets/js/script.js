9ec096d13c92c07cbe48e4924f9c1315c06d6af7
Yvonne <yowantings@gmail.com>
2024-05-11 20:45:00 -0600

    added scrpit js and a package-json-lock

---
+++ Develop/public/assets/js/script.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const db = require('./db/db.json')
+
